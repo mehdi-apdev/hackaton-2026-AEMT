ef24b31ca94aedec0b340849ae383109682e0097
Flexxss <vincart.kurtis@hotmail.com>
2026-01-19 14:49:38 +0100

    style: Routing
    Routing revamped

---
+++ frontend/src/HomeComponent.tsx
@@ -0,0 +1,8 @@
+export default function HomeComponent() {
+  return (
+    <section style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Bienvenue sur l'application</h1>
+      <p>Chargement du contenu en cours, veuillez patienter...</p>
+    </section>
+  );
+}
