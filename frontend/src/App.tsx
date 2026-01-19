c9388dee0988be49cc57e0f7befcdf275b887a9d
Flexxss <vincart.kurtis@hotmail.com>
2026-01-19 14:55:25 +0100

    style: Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import NotFoundComponent from "./core/components/NotFoundComponent";
 
-import systemRoutes from "./features/system/system-routes";
+import { systemRoutes } from "./features/system/system-routes";
 import NotesPage from "./features/notes/pages/NotesPage";
-import HomeComponent from "./HomeComponent";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        {/* Route d'accueil par défaut */}
-        <Route index element={<HomeComponent />} />
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        // feature "system"
+        ...systemRoutes,
 
-        {/* feature "system" */}
-        {systemRoutes}
+        // feature "notes"
+        { path: "notes", element: <NotesPage /> },
 
-        {/* feature "notes" */}
-        <Route path="notes" element={<NotesPage />} />
+        // 404 (toujours en dernier)
+        { path: "*", element: <NotFoundComponent /> },
+      ],
+    },
+  ]);
 
-        {/* 404 */}
-        <Route path="*" element={<NotFoundComponent />} />
-      </Route>
-    </Routes>
-  );
+  return element;
 }
