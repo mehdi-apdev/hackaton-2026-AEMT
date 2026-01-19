4c8f33b4856cdfe8c4c478e8975de70ede34aca8
Play <lou.taquin@gmail.com>
2026-01-19 10:33:11 +0100

    feat markdown
    v0.1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import NotFoundComponent from "./core/components/NotFoundComponent";
-import systemRoutes from "./features/system/system-routes";
+
+import { systemRoutes } from "./features/system/system-routes";
 
 function App() {
   return (
     <Routes>
-      {/* Route principale avec le layout global */}
       <Route path="/" element={<MainLayout />}>
-        
-        {/* Les routes de la feature "system" sont injectées ici */}
-        {systemRoutes}
+        {/* system routes */}
+        <Route path="ping" element={systemRoutes[0].element} />
+        <Route path="markdown" element={systemRoutes[1].element} />
 
-        {/* Ajoutez d'autres features ici */}
-        {/* {featureXYZRoutes} */}
-
-        {/* La page 404 est la dernière route pour tout intercepter */}
         <Route path="*" element={<NotFoundComponent />} />
       </Route>
     </Routes>
--- frontend/src/features/system/system-routes.tsx
+++ frontend/src/features/system/system-routes.tsx
@@ -1,10 +1,16 @@
-import { Route } from "react-router-dom";
+import type { RouteObject } from "react-router-dom";
 import PingTest from "./components/PingTest";
+import MarkdownPage from "./components/MarkdownPage";
 
 // Le chemin est relatif au layout dans lequel ces routes seront imbriquées.
 // Si le layout est à la racine "/", alors ce chemin sera bien "/ping".
-const systemRoutes = [
-    <Route key="ping" path="/ping" element={<PingTest />} />
+export const systemRoutes: RouteObject[] = [
+  {
+    path: "ping",
+    element: <PingTest />,
+  },
+  {
+    path: "markdown",
+    element: <MarkdownPage />,
+  },
 ];
-
-export default systemRoutes;
