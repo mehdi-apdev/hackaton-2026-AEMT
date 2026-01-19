95cec48fc6dcc36224e20a8c2146a46429f57a95
Flexxss <vincart.kurtis@hotmail.com>
2026-01-19 11:18:40 +0100

    Implemented sidebar with styles
    Brand new sidebar with :
    - title
    - can be collapsed
    - styles (not the final ones)

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,16 +1,37 @@
-import { Outlet, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import LeftSidebar from "../features/system/components/left-sidebar/LeftSidebar";
 import "./MainLayout.css";
+import classNames from "classnames";
 
 const MainLayout = () => {
+  const [isLeftSidebarCollapsed, setIsLeftSidebarCollapsed] = useState<boolean>(false);
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const updateSize = () => {
+      setScreenWidth(window.innerWidth);
+      if (window.innerWidth < 768) {
+        setIsLeftSidebarCollapsed(true);
+      }
+    };
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  const contentClasses = classNames({
+    "layout-content": true,
+    "layout-content-trimmed": !isLeftSidebarCollapsed && screenWidth > 768,
+  });
+
   return (
     <div>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="nav-link">Accueil</Link>
-          <Link to="/ping" className="nav-link">Ping</Link>
-        </div>
-      </nav>
-      <main className="main-content">
+      <LeftSidebar
+        isLeftSidebarCollapsed={isLeftSidebarCollapsed}
+        changeIsLeftSidebarCollapsed={setIsLeftSidebarCollapsed}
+      />
+      <main className={contentClasses}>
         <Outlet />
       </main>
     </div>
